437f21bf255bed0fad007c50d739e2e3efc138a6
QuocKhanh2903 <bluegameming292003@gmail.com>
2023-11-01 00:51:30 +0700

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,6 +7,11 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Contact from './ContactComponent';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
 
 
 class Main extends Component {
@@ -14,8 +19,11 @@ class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        dishes: DISHES,
-        selectedDish: null
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
+
     };
   }
 
@@ -26,8 +34,12 @@ class Main extends Component {
   render() {
     const HomePage = () => {
       return(
-          <Home 
-          />
+        <Home 
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+
       );
     }
 
@@ -43,6 +55,7 @@ class Main extends Component {
         <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
         <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
         <Footer />
+        <Route exact path='/contactus' Component={Contact} />
 
       </div>
     );
